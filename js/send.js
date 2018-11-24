c3e937e6691ed45328c31e3c16a66215dfaedd33
xuan525 <shanboxuan@me.com>
2018-11-24 00:03:59 +0000

    Add base64 convert

---
+++ js/send.js
@@ -0,0 +1,12 @@
+function send() {
+    //convert to base64
+    document.getElementById('msgTitle').value = btoa(document.getElementById('msgTitle').value);
+    document.getElementById('msgName').value = btoa(document.getElementById('msgName').value);
+    document.getElementById('msgContent').value = btoa(document.getElementById('msgContent').value);
+    //submit
+    document.getElementById('msgForm').submit();
+    //convert back to text
+    document.getElementById('msgTitle').value = atob(document.getElementById('msgTitle').value);
+    document.getElementById('msgName').value = atob(document.getElementById('msgName').value);
+    document.getElementById('msgContent').value = atob(document.getElementById('msgContent').value);
+}
